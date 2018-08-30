2fe2e8cff9d7f8a693c7a9d9907982a741e0958c
Kaspars Dambis <hi@kaspars.net>
2018-08-30 12:55:31 +0300

    Fix the command

--- src/ReplicatorCommand.php
+++ src/ReplicatorCommand.php
@@ -3,10 +3,11 @@
 namespace WPSH_Replicator;
 
 use WP_CLI;
+use WP_CLI_Command;
 
 /**
  * Defines the Replicator WP CLI command.
  */
-class ReplicatorCommand {
+class ReplicatorCommand extends WP_CLI_Command {
 
 }
